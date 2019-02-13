76102993ef582f8f4f64fc4768b14e20897c68e6
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-02-13 02:19:10 +0200

    refactor first test

--- cypress/integration/duckduckgo.spec.js
+++ cypress/integration/duckduckgo.spec.js
@@ -1,5 +1,13 @@
 describe('DuckDuckGo main page', () => {
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+
     it('Search field exist', () => {
-cy.visit('http://duckduckgo.com');
+        const inputCssSelector = '#search_form_input_homepage'
+        const text = 'Hello, World'
+      cy.get('.content-wrap--home').should('be.visible')
+      cy.get(inputCssSelector).type(text, {force: true})
     })
 })
