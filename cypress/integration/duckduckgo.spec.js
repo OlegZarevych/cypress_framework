79a0218babd2671beb9b07f64ba96c3308bbd139
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-02-20 01:06:03 +0200

    add new test

--- cypress/integration/duckduckgo.spec.js
+++ cypress/integration/duckduckgo.spec.js
@@ -1,15 +1,22 @@
 describe('DuckDuckGo main page', () => {
+    const inputCssSelector = '#search_form_input_homepage'
+    const text = 'Hello, World'
+
     beforeEach(() => {
         cy.visit('/')
     })
 
 
     it('Search field exist', () => {
-        const inputCssSelector = '#search_form_input_homepage'
-        const text = 'Hello, World'
       cy.get('.content-wrap--home').should('be.visible')
       cy.get(inputCssSelector).type(text, {force: true})
       cy.get('#search_button_homepage').click({force: true})
       cy.get('.results--main').should('exist')
+    })
+
+    it('Search return other response', () => {
+        cy.server()
+        cy.route('GET', '/ac/?callback=autocompleteCallback&q=Hello%2C+World&**',{})
+        cy.get(inputCssSelector).type(text, {force: true})
     })
 })
